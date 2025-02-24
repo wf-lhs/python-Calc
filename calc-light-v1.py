67e5791cd9175a7fe24a8544112a33dddd0ecd22
wf-lhs <15flewil01@larberthighschool.co.uk>
2025-02-24 08:45:56 +0000

    Add files via upload

---
+++ calc-light-v1.py
@@ -0,0 +1,34 @@
+print("""
+input at the mode section
+1 = addition
+2 = subtraction
+3 = multiplication
+4 = division
+""")
+print("mode")
+mode = int(input())
+if mode == 1:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"+",ii,"=",i+ii)
+if mode == 2:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"-",ii,"=",i-ii)
+if mode == 1:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"x",ii,"=",i*ii)
+if mode == 4:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"/",ii,"=",i/ii)
+
