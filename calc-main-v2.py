b9bfdfa612b56c61877af6ab762c2157d21997b3
wf-lhs <15flewil01@larberthighschool.co.uk>
2025-02-25 11:02:57 +0000

    Add files via upload

---
+++ calc-main-v2.py
@@ -0,0 +1,49 @@
+print("""
+input at the mode section
+1 = addition
+2 = subtraction
+3 = multiplication
+4 = division
+5 = long addition
+6 = power / exponent
+""")
+print("mode")
+mode = int(input())
+if mode == 1:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"+",ii,"=",i+ii)
+if mode == 2:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"-",ii,"=",i-ii)
+if mode == 3:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"x",ii,"=",i*ii)
+if mode == 4:
+    print("num 1")
+    i = float(input())
+    print("num 2")
+    ii = float(input())
+    print(i,"/",ii,"=",i/ii)
+if mode == 5:
+    numcount = int(input('numcount: '))
+    ii = 0
+    iii = 0
+    while ii < numcount:
+        i = int(input('num:'))
+        iii = iii+i
+        ii = ii+1
+        print("total =",iii)
+if mode == 6:
+    i = int(input("number:"))
+    ii = int(input("to the power of:"))
+    iii = i**ii
+    print(iii)
