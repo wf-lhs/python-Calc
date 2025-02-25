f4d42ae2037c8afc43e6a4edfec70d98a2361d15
wf-lhs <15flewil01@larberthighschool.co.uk>
2025-02-25 11:18:07 +0000

    Update calc-main-v2.py

--- calc-main-v2.py
+++ calc-main-v2.py
@@ -1,5 +1,6 @@
 print("""
 input at the mode section
+0 = quit
 1 = addition
 2 = subtraction
 3 = multiplication
@@ -9,31 +10,34 @@ input at the mode section
 """)
 print("mode")
 mode = int(input())
-if mode == 1:
+if mode == 0:
+    print("quitting")
+    quit()
+if mode == 1:#addition
     print("num 1")
     i = float(input())
     print("num 2")
     ii = float(input())
     print(i,"+",ii,"=",i+ii)
-if mode == 2:
+if mode == 2:#subtraction
     print("num 1")
     i = float(input())
     print("num 2")
     ii = float(input())
     print(i,"-",ii,"=",i-ii)
-if mode == 3:
+if mode == 3:#multiplication
     print("num 1")
     i = float(input())
     print("num 2")
     ii = float(input())
     print(i,"x",ii,"=",i*ii)
-if mode == 4:
+if mode == 4:#devision
     print("num 1")
     i = float(input())
     print("num 2")
     ii = float(input())
     print(i,"/",ii,"=",i/ii)
-if mode == 5:
+if mode == 5:#addition
     numcount = int(input('numcount: '))
     ii = 0
     iii = 0
@@ -42,7 +46,7 @@ if mode == 5:
         iii = iii+i
         ii = ii+1
         print("total =",iii)
-if mode == 6:
+if mode == 6: #power,exponent
     i = int(input("number:"))
     ii = int(input("to the power of:"))
     iii = i**ii
